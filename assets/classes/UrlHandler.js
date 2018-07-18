11caf9cc744fb9367a0c5bea7eb6472b1469a95b
Mike <m.consemulder1992@gmail.com>
2018-07-18 17:49:40 +0200

    added check for credentials

--- assets/classes/UrlHandler.js
+++ assets/classes/UrlHandler.js
@@ -1,37 +1,45 @@
-let credentials = require("../../credentials/credentials.json");
+try {
+    let credentials = require("../../credentials/credentials.json");
+    exportModule(credentials);
+} catch (ex) {
+    console.log('it didnt work', ex);
+}
 
-module.exports = class UrlHandler {
+function exportModule(credentials) {
 
-    constructor(urlObject) {
+    module.exports = class UrlHandler {
 
-        //private vars
-        let requestObject = {
-            baseUrl: 'https://api.untappd.com',
-            version: 'v4',
-            requestCredentials: credentials,
-            endpoint: urlObject.endpoint,
-            username: urlObject.username,
-            params: urlObject.params
-        }
+        constructor(urlObject) {
 
-        //create the url
-        this.url = this.constructRequestUrl(requestObject);
-    }
-
-    constructRequestUrl(requestObject) {
-
-        let url = `${requestObject.baseUrl}/${requestObject.version}/${requestObject.endpoint}/${requestObject.username}?`;
-        url += `client_id=${requestObject.requestCredentials.client_id}&client_secret=${requestObject.requestCredentials.client_secret}`;
-
-        if (typeof requestObject.params !== 'undefined') {
-
-            let objectKeys = Object.keys(requestObject.params);
-            for (let i = 0; i < objectKeys.length; i++) {
-
-                url += `&${objectKeys[i]}=${requestObject.params[objectKeys[i]]}`;
+            //private vars
+            let requestObject = {
+                baseUrl: 'https://api.untappd.com',
+                version: 'v4',
+                requestCredentials: credentials,
+                endpoint: urlObject.endpoint,
+                username: urlObject.username,
+                params: urlObject.params
             }
+
+            //create the url
+            this.url = this.constructRequestUrl(requestObject);
         }
 
-        return url;
+        constructRequestUrl(requestObject) {
+
+            let url = `${requestObject.baseUrl}/${requestObject.version}/${requestObject.endpoint}/${requestObject.username}?`;
+            url += `client_id=${requestObject.requestCredentials.client_id}&client_secret=${requestObject.requestCredentials.client_secret}`;
+
+            if (typeof requestObject.params !== 'undefined') {
+
+                let objectKeys = Object.keys(requestObject.params);
+                for (let i = 0; i < objectKeys.length; i++) {
+
+                    url += `&${objectKeys[i]}=${requestObject.params[objectKeys[i]]}`;
+                }
+            }
+
+            return url;
+        }
     }
 }
