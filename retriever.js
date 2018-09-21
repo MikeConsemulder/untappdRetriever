4ca246d1d4b0795207ed5dfab71ae19eebd98e36
Mike <m.consemulder1992@gmail.com>
2018-09-21 16:01:38 +0200

    test

--- retriever.js
+++ retriever.js
@@ -13,14 +13,6 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//set header.. not sure if smart :)
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Lang");
-    next();
-});
-
 //set basic authentication
 app.use(basicAuth({
     authorizer: authorizer,
