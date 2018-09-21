f98b897826fc85996df1e8274e5a8b6d0ae66de5
Mike <m.consemulder1992@gmail.com>
2018-09-21 15:58:30 +0200

    yes yes do it

--- retriever.js
+++ retriever.js
@@ -13,17 +13,19 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//set header.. not sure if smart :)
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Lang");
+    next();
+});
+
 //set basic authentication
 app.use(basicAuth({
     authorizer: authorizer,
     authorizeAsync: true
 }))
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 function authorizer(username, password, cb) {
     //get username and password from .env
